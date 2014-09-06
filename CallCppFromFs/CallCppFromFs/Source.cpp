04326ef48ab7031e20db0a00b4b3e5583715a3ef
teramonagi <teramonagi@gmail.com>
2014-09-06 11:55:25 +0900

    Add memory allocate and release

--- CallCppFromFs/CallCppFromFs/Source.cpp
+++ CallCppFromFs/CallCppFromFs/Source.cpp
@@ -1,7 +1,24 @@
 #include <numeric>      // std::accumulate
-//add for integer
+//simple value
 extern "C" __declspec(dllexport) int __cdecl addI(int x, int y){ return x + y; }
-//add for double
 extern "C" __declspec(dllexport) double __cdecl addD(double x, double y){ return x+y; }
-//
+//float array sum
 extern "C" __declspec(dllexport) double __cdecl arraySum(double *x, int size){ return std::accumulate(x, x + size, 0.0); }
+//string
+extern "C" __declspec(dllexport) char __cdecl string1(char x){ return ++x; }
+extern "C" __declspec(dllexport) int __cdecl string2(char* x)
+{ 
+	if (x[0] == 'H')
+	{
+		return 1;
+	}
+	return 0;
+}
+//allocate and release memory 
+extern "C" __declspec(dllexport) double* __cdecl allocate_memory(int size)
+{
+	double *res = new double[size];
+	for (int i = 0; i < size; ++i){ res[i] = i; }
+	return res;
+}
+extern "C" __declspec(dllexport) void __cdecl release_memory(double *x){delete[] x;}
--- CallCppFromFs/Script/Script.fsx
+++ CallCppFromFs/Script/Script.fsx
@@ -1,5 +1,7 @@
 ﻿open System
 open System.Runtime.InteropServices
+open Microsoft.FSharp.Core
+open Microsoft.FSharp.NativeInterop 
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 //Import declaration
 [<DllImport(@"../Debug/CppDLL.dll", CallingConvention = CallingConvention.Cdecl)>]
@@ -8,8 +10,23 @@ extern int addI(int x, int y)
 extern float addD(float x, float y)
 [<DllImport(@"../Debug/CppDLL.dll", CallingConvention = CallingConvention.Cdecl)>]
 extern float arraySum(float[] x, int size)
-
+[<DllImport(@"../Debug/CppDLL.dll", CallingConvention = CallingConvention.Cdecl)>]
+extern char string1(char x)
+[<DllImport(@"../Debug/CppDLL.dll", CallingConvention = CallingConvention.Cdecl)>]
+extern int string2(string x)
+[<DllImport(@"../Debug/CppDLL.dll", CallingConvention = CallingConvention.Cdecl)>]
+extern nativeint allocate_memory(int size)
+[<DllImport(@"../Debug/CppDLL.dll", CallingConvention = CallingConvention.Cdecl)>]
+extern void release_memory(double *x)
+//test code
 addI(1, 2)
 addD(1.5, 2.5)
 [|1.0; 2.0; 3.0;|] |> fun x -> arraySum(x, x.Length)
-
+'A' |> string1
+"Hello" |> string2 
+"Aello" |> string2 
+//memory allocation, get value, and release it.
+let ptr : nativeptr<double> = NativePtr.ofNativeInt(allocate_memory(10))
+[0..9] |> List.iter (fun i -> printfn "%f" (NativePtr.get ptr i))
+release_memory ptr
+[0..9] |> List.iter (fun i -> printfn "%f" (NativePtr.get ptr i)) //alread released...
